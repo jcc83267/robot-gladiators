f29b1521687adbae67c09c66f7f3c43771e58620
James Canlas <jamescanlasjr@yahoo.com>
2021-01-02 20:24:53 -0800

    added highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -183,11 +183,19 @@ var startGame=function() {
 //function to end the entire game
 var endGame=function() {
     window.alert("The game has now ended. Let's see how you did!");
-    if (playerInfo.health > 0) { 
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+    // if player have more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } 
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
     var playAgainConfirm=window.confirm("Would you like to play again?") 
     if (playAgainConfirm) {
@@ -197,6 +205,7 @@ var endGame=function() {
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
+
 }
 
 //function for shop
